40335cc81225b7e9fdd3833b42d0a4f1a3848173
Chloe-O <chloeosbourne@yahoo.co.uk>
2025-07-01 21:58:13 +0100

    functions to establish set or custom tip

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 let tipBtns = document.getElementsByClassName("tip-btn");
 let selectTip = document.getElementById("selectTip");
+let customTip = document.getElementById("customTipInput");
+
+let testBox = document.getElementById("test-box");
 
 let tipAmount;
+let customTipAmount;
 
-selectTip.addEventListener('click', (e) => {
-    // console.log(e.target.value);
-    tipAmount = e.target.value;
-    console.log(tipAmount);
-});
+function checkTipByClick() {
+  selectTip.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tip-btn")) {
+      tipAmount = e.target.value;
+      customTip.value = "";
+    }
+  });
+}
+
+function checkTipByCustom() {
+    customTip.addEventListener('input', () => {
+       testBox.textContent = customTip.value;
+       customTipAmount = customTip.value;
+       console.log(`The value of customTipAmount is ${customTipAmount}`);
+    })
+}
+
+checkTipByClick();
+checkTipByCustom();
 
 
 // store value of tip btn clicked on OR IF there is a valid number in the custom tip input box, use that as the tip...must be more than 0 and field not blank
