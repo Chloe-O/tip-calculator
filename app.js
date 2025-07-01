5c33e7027e09ef35b10e9b13bb154208d7c919eb
Chloe-O <chloeosbourne@yahoo.co.uk>
2025-07-01 22:22:07 +0100

    added variables for partysize and bill amount

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 let tipBtns = document.getElementsByClassName("tip-btn");
 let selectTip = document.getElementById("selectTip");
 let customTip = document.getElementById("customTipInput");
+let billInput = document.getElementById("billInput");
+let partySize = document.getElementById("partySize");
 
 let testBox = document.getElementById("test-box");
 
-let tipAmount;
+let selectTipAmount;
 let customTipAmount;
+let billAmount;
 
 function checkTipByClick() {
   selectTip.addEventListener("click", (e) => {
     if (e.target.classList.contains("tip-btn")) {
-      tipAmount = e.target.value;
+      selectTipAmount = e.target.value;
       customTip.value = "";
     }
   });
 }
 
 function checkTipByCustom() {
-    customTip.addEventListener('input', () => {
-       testBox.textContent = customTip.value;
-       customTipAmount = customTip.value;
-       console.log(`The value of customTipAmount is ${customTipAmount}`);
-    })
+  customTip.addEventListener("input", () => {
+    testBox.textContent = customTip.value;
+    customTipAmount = customTip.value;
+    console.log(`The value of customTipAmount is ${customTipAmount}`);
+  });
+};
+
+function calculateTotalBill() {
+    
 }
 
 checkTipByClick();
 checkTipByCustom();
-
-
-// store value of tip btn clicked on OR IF there is a valid number in the custom tip input box, use that as the tip...must be more than 0 and field not blank
